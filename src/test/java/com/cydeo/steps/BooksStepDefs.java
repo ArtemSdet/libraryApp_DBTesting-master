b995f2a721aec25db9bce42d0d02953d5caf8bf2
ArtemSdet <102181522+ArtemSdet@users.noreply.github.com>
2022-08-06 17:24:01 -0600

    Add files via upload

---
+++ src/test/java/com/cydeo/steps/BooksStepDefs.java
@@ -0,0 +1,108 @@
+package com.cydeo.steps;
+
+import com.cydeo.pages.BookPage;
+import com.cydeo.pages.DashBoardPage;
+import com.cydeo.utility.BrowserUtil;
+import com.cydeo.utility.DB_Util;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class BooksStepDefs {
+    BookPage bookPage=new BookPage();
+    List<String> actualCategoryList;
+
+
+    @When("the user navigates to {string} page")
+    public void the_user_navigates_to_page(String moduleName) {
+        new DashBoardPage().navigateModule(moduleName);
+
+
+    }
+
+
+    @When("the user gets all book categories in webpage")
+    public void the_user_gets_all_book_categories_in_webpage() {
+        actualCategoryList=BrowserUtil.getAllSelectOptions(bookPage.mainCategoryElement);
+        actualCategoryList.remove(0);
+        System.out.println("actualCategoryList = " + actualCategoryList);
+    }
+
+    @Then("user should be able to see following categories")
+    public void user_should_be_able_to_see_following_categories(List<String> expectedCategoryList) {
+
+
+        Assert.assertEquals(expectedCategoryList, actualCategoryList);
+
+    }
+
+
+    @When("I open book {string}")
+    public void i_open_book(String bookName) {
+
+        System.out.println("bookName = " + bookName);
+        BrowserUtil.waitForClickablility(bookPage.search, 5).sendKeys(bookName);
+        BrowserUtil.waitForClickablility(bookPage.editBook(bookName), 5).click();
+
+    }
+
+    @Then("verify book categories must match book categories table from db")
+    public void verify_book_categories_must_match_book_categories_table_from_db() {
+
+        String query="select name from book_categories";
+
+        DB_Util.runQuery(query);
+
+        //store data
+        List<String> expectedCategoryList = DB_Util.getColumnDataAsList(1);
+
+
+
+        //Assertions
+        Assert.assertEquals(expectedCategoryList,actualCategoryList);
+
+
+    }
+
+    @Then("book information must match the database for {string}")
+    public void book_information_must_match_the_database_for(String bookName) {
+
+        BrowserUtil.waitFor(3);
+
+        //get data from UI
+
+        String actualBookName = bookPage.bookName.getAttribute("value");
+        String actualAuthorName = bookPage.author.getAttribute("value");
+        String actualISBN = bookPage.isbn.getAttribute("value");
+        String actualYear = bookPage.year.getAttribute("value");
+        String actualDesc = bookPage.description.getAttribute("value");
+
+        //get related book info from DB
+        String query="select name,author,isbn,description,year from books where name ='"+bookName+"'";
+
+        DB_Util.runQuery(query);
+
+        Map<String, String> rowMap = DB_Util.getRowMap(1);
+
+        String expectedBookName = rowMap.get("name");
+        String expectedAuthorName = rowMap.get("author");
+        String expectedISBN = rowMap.get("isbn");
+        String expectedDesc = rowMap.get("description");
+        String expectedYear = rowMap.get("year");
+
+        //Assertion
+
+        Assert.assertEquals(expectedBookName,actualBookName);
+        Assert.assertEquals(expectedAuthorName,actualAuthorName);
+        Assert.assertEquals(expectedISBN,actualISBN);
+        Assert.assertEquals(expectedDesc,actualDesc);
+        Assert.assertEquals(expectedYear,actualYear);
+
+
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/steps/DashboardStepDefs.java
@@ -0,0 +1,83 @@
+package com.cydeo.steps;
+
+import com.cydeo.pages.DashBoardPage;
+import com.cydeo.pages.LoginPage;
+import com.cydeo.utility.BrowserUtil;
+import com.cydeo.utility.DB_Util;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class DashboardStepDefs
+{
+    String actualUserNumbers;
+    String actualBookNumbers;
+    String actualBorrowedBookNumbers;
+    LoginPage loginPage=new LoginPage();
+    DashBoardPage dashBoardPage=new DashBoardPage();
+
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String user) {
+        loginPage.login(user);
+         BrowserUtil.waitFor(4);
+    }
+    @When("user gets all information from modules")
+    public void user_gets_all_information_from_modules() {
+
+        actualUserNumbers = dashBoardPage.usersNumber.getText();
+        System.out.println("actualUserNumbers = " + actualUserNumbers);
+        actualBookNumbers = dashBoardPage.booksNumber.getText();
+        System.out.println("actualBookNumbers = " + actualBookNumbers);
+        actualBorrowedBookNumbers = dashBoardPage.borrowedBooksNumber.getText();
+        System.out.println("actualBorrowedBookNumbers = " + actualBorrowedBookNumbers);
+
+    }
+    @Then("the informations should be same with database")
+    public void the_informations_should_be_same_with_database() {
+
+        //1.Make a conn
+        // DB_Util.createConnection();
+        // since we have Before After with custom hooks we dont need this step any more
+
+        //USERS
+
+                //2.Run Query
+                DB_Util.runQuery("select count(*) from users");
+
+                //3.Store data
+                String expectedUsers = DB_Util.getFirstRowFirstColumn();
+
+                //4.Make an assertion
+                Assert.assertEquals(expectedUsers,actualUserNumbers);
+
+
+        //BOOKS
+
+
+            //2.Run Query
+                DB_Util.runQuery("select count(*) from books");
+            //3.Store data
+                 String expectedBooks = DB_Util.getCellValue(1, 1);
+            //4.Make an assertion
+                Assert.assertEquals(expectedBooks,actualBookNumbers);
+
+        //BORROWED BOOKS
+
+            //2.Run Query
+                DB_Util.runQuery("select count(*) from book_borrow where is_returned=0");
+            //3.Store data
+                String expectedBookBorrowedNumber = DB_Util.getFirstRowFirstColumn();
+            //4.Make an assertion
+                Assert.assertEquals(expectedBookBorrowedNumber,actualBorrowedBookNumbers);
+
+        //5.Close the conn
+        //DB_Util.destroy();
+        // since we have Before After with custom hooks we dont need this step any more
+
+
+    }
+
+
+}
